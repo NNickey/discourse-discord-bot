171bccab73282995df36426e6ce6aba931b2812f
NickoSCP <nicenickey123@gmail.com>
2022-05-12 08:23:08 -0600

    Update bot_commands.rb

--- lib/bot_commands.rb
+++ lib/bot_commands.rb
@@ -29,7 +29,7 @@ module ::DiscordBot::BotCommands
         #posting_user = system_user
         event.user.add_role(972553176035250286, reason = 'Verified by Discord Link Bot')
       else
-        m.edit "Your account is not found. Be sure you click on sign up on the fremont forum, fremont.nixplugs.click, and one click sign up/in."
+        m.edit "Your account is not found. Be sure you click on sign up on the fremont forum, https://fremont.nixplugs.click/signup , click on authorize for discord, and then try the command again."
       end
       
       
